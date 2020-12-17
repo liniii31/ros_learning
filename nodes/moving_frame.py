c8fe173dc9ceacf614d1527a957bf89a16813930
Ryosuke Tajima <tajima.ryosuke@techmagic.co.jp>
2020-12-17 18:01:40 +0900

    One modification example for issue 1 and 2
    - Please notice this is base on the tutorial code (http://wiki.ros.org/ROS/Tutorials/WritingPublisherSubscriber%28python%29)

--- nodes/moving_frame.py
+++ nodes/moving_frame.py
@@ -1,50 +1,46 @@
-#!/usr/bin/env python  
-
+#!/usr/bin/env python
+# license removed for brevity
 import rospy
-
-# Because of transformations
-import tf_conversions
 import math
 import tf2_ros
+import tf_conversions
 import geometry_msgs.msg
-import turtlesim.msg
-from ros_learning.srv import *
 
-
-
-def handle_turtle_pose(msg):
+def publisher():
     br = tf2_ros.TransformBroadcaster()
     t = geometry_msgs.msg.TransformStamped()
-    t.header.stamp = rospy.Time.now()
-    t.header.frame_id = "map"
-    t.child_frame_id = "base_link"
-    t.transform.translation.x = msg.x
-    t.transform.translation.y = msg.y
-    t.transform.translation.z = 0.0
-    q = tf_conversions.transformations.quaternion_from_euler(0, 0, msg.theta)
-    t.transform.rotation.x = q[0]
-    t.transform.rotation.y = q[1]
-    t.transform.rotation.z = q[2]
-    t.transform.rotation.w = q[3]
-    br.sendTransform(t)
-
-
-def handle(req):
-    rospy.Subscriber('/turtle1/pose',
-                     turtlesim.msg.Pose,
-                     handle_turtle_pose)
-    rospy.spin()  
-           
     
-
-def revolve():
-    s = rospy.Service('move_in_circle', srv_msg, handle)
-    
-    rospy.spin()
-
+    # periodic rate: 10 [hz]
+    rate = rospy.Rate(10)
+    # timer counter [second], increase 0.01 in each period
+    tick = 0
+    # angular velocity [rad/s]
+    omega = 1.0
+    while not rospy.is_shutdown():
+        t.header.stamp = rospy.Time.now()
+        t.header.frame_id = "map"
+        t.child_frame_id = "base_link"
+        # This is target angle around z axis
+        theta = omega*tick
+        t.transform.translation.x = math.sin(theta)
+        t.transform.translation.y = math.cos(theta)
+        t.transform.translation.z = 0
+        # The orientation should be set as you did
+        # set it negative because we want to make x-xaxis to frontal direction
+        q = tf_conversions.transformations.quaternion_from_euler(0, 0, -theta)
+        t.transform.rotation.x = q[0]
+        t.transform.rotation.y = q[1]
+        t.transform.rotation.z = q[2]
+        t.transform.rotation.w = q[3]
+        br.sendTransform(t)
+        # Next period
+        tick += 0.01
+        rate.sleep()
 
 if __name__ == '__main__':
-    rospy.init_node('moving_frame')
-    revolve()
-    
+    rospy.init_node('publisher', anonymous=True)
 
+    try:
+        publisher()
+    except rospy.ROSInterruptException:
+        pass
